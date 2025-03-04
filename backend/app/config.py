75e400f9e496b16b0f9382d9671a124060c67d6c
sheena849 <sheenamugo04@gmail.com>
2025-03-04 11:09:04 +0300

    Changes in mail

--- backend/app/config.py
+++ backend/app/config.py
@@ -19,9 +19,9 @@ class Config:
     MAIL_PORT = 587  
     MAIL_USE_TLS = True  
     MAIL_USE_SSL = False  
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')  
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')  
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+    MAIL_USERNAME=sheenamugo04@gmail.com
+    MAIL_PASSWORD=krcoclzzzzjbnmqh  # Use your generated App Password (no spaces)
+    MAIL_DEFAULT_SENDER=sheenamugo04@gmail.com
 
 class DevelopmentConfig(Config):
     DEBUG = True
