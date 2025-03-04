41662376574a0600d816028c127388782eb2f15d
sheena849 <sheenamugo04@gmail.com>
2025-03-04 09:51:48 +0300

    updated database uri and requirements

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Config:
     
 
-    SQLALCHEMY_DATABASE_URI = os.get.environ('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
